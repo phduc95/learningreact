d17d7fc375d97d70d1ba660a0bc0a90e5ca2c904
DucPham <phduc95@gmail.com>
2018-09-06 10:32:21 +0700

    finish code 2.2

--- server.js
+++ server.js
@@ -1,5 +1,25 @@
+import http from 'http';
 
-import config,{nodeEnv,logStars} from './config';
-logStars('DUC PHAM');
+const server=http.createServer( (req, res)=>{
+    res.write("Duc Pham, Testing");
+    setTimeout(()=>{
+        res.write(
+            'I can Fuck'
+        );
+        res.end();
+    },3000);});
+server.listen(8080);
+// import https from 'https'
+// https.get('https://www.youtube.com',res=>{
+//     console.log('Response status code: ',res.statusCode);
+//
+//     res.on('data',chunk=>{
+//         console.log(chunk.toString());
+//     })
+// });
 
-// console.log(config,nodeEnv);
+//
+// import config,{nodeEnv,logStars} from './config';
+// logStars('DUC PHAM');
+//
+// // console.log(config,nodeEnv);
